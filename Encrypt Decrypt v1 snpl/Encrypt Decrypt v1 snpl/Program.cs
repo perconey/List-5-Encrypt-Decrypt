855caca828352c7acd62d91fc62b38c34459b0dd
Perconey <perconey@gmail.com>
2017-10-15 13:09:25 +0200

    Source code

---
+++ Encrypt Decrypt v1 snpl/Encrypt Decrypt v1 snpl/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encrypt_Decrypt_v1_snpl
+{
+    /// <summary>
+    /// Simple algorithm which takes user input and returns encrypted string. Same for decryption
+    /// </summary>
+    class Program
+    {
+        // Functions below are mainly taken from .net reference from ms
+        static void Main(string[] args)
+        {
+            try
+            {
+                //gets user input
+                var input = Console.ReadLine();
+                Aes myAes = Aes.Create();
+
+                byte[] encrypted = EncryptStringToBytes_Aes(input, myAes.Key, myAes.IV);
+
+                // Decrypt the bytes to a string.
+                string dec = DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
+
+                //Show items in array
+                foreach(var item in encrypted)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("Before: " + input);
+                Console.WriteLine("After: " + dec);
+                Console.ReadKey();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        static byte[] EncryptStringToBytes_Aes(string text, byte[] Key, byte[] IV)
+        {
+
+            if (text == null || text.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key , aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt , encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(text);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            return encrypted;
+
+        }
+
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            string plaintext = null;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                             plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
+        }
+    }
+}
+
